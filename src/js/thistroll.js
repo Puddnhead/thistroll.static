61f64f3ff6f7b00a84681def4157f83043fb59c6
Mike Wilklow <michaelwilklow@gmail.com>
2017-07-21 14:56:21 -0500

    Bloglist and Troll

--- src/js/thistroll.js
+++ src/js/thistroll.js
@@ -1,8 +1,8 @@
 var troll = {
 
   // location of the ec2 server
-  serverHost: "http://thistroll.us-east-2.elasticbeanstalk.com",
-  // serverHost: "http://localhost:8081",
+  //serverHost: "http://thistroll.us-east-2.elasticbeanstalk.com",
+   serverHost: "http://localhost:8081",
 
   loadCurrentBlog: function () {
     var blogTitleH1 = document.getElementById("blogTitle"),
@@ -15,5 +15,19 @@ var troll = {
       blogDateH2.innerHTML = blog.createdOn ? new Date(blog.createdOn).toLocaleDateString() : "";
       blogTextDiv.innerHTML = blog.text;
     });
+  },
+
+  loadBlogList: function () {
+    var endpoint = this.serverHost + "/blog/all",
+      blogList = document.getElementById("blogList"),
+      index,
+      blogCount;
+
+    $.get(endpoint, function (blogs) {
+      for (index = 0, blogCount = blogs.length; index < blogCount; index++) {
+        blogList.innerHTML += "<li><a href='index.html?blog=" +
+          blogs[index].id + "'>" + blogs[index].title + "</a></li>";
+      }
+    });
   }
-}
+};
